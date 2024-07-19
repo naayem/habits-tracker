6510d89f014e4425d5da7551c101e31d250c6f6a
naayem <vincentnaayem@gmail.com>
2024-07-19 23:02:38 +0300

    try except from load yaml

--- habits_tracker/utils.py
+++ habits_tracker/utils.py
@@ -17,10 +17,13 @@ def load_yaml_as_dict(config_path: str) -> dict:
 
 
 def load_env_from_yaml(yaml_path):
-    with open(yaml_path, 'r') as f:
-        config = yaml.safe_load(f)
-        for key, value in config.items():
-            os.environ[key] = str(value)
+    try:
+        with open(yaml_path, 'r') as f:
+            config = yaml.safe_load(f)
+            for key, value in config.items():
+                os.environ[key] = str(value)
+    except FileNotFoundError as fnf_error:
+        print(fnf_error)
 
 # Example usage
 # load_env_from_yaml(".secrets.yaml")
